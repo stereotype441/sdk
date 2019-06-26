66929806e3807a09c61d31a6b78cb3776c5269ae
Paul Berry <paulberry@google.com>
2019-06-26 11:33:49 -0700

    WIP

--- pkg/nnbd_migration/test/node_builder_test.dart
+++ pkg/nnbd_migration/test/node_builder_test.dart
@@ -345,6 +345,13 @@ class C {
         same(decoratedType));
   }
 
+  solo_test_generic_function_simple() async {
+    await analyze('''
+void f<T>() {}
+''');
+    fail('TODO(paulberry)');
+  }
+
   test_genericFunctionType_namedParameterType() async {
     await analyze('''
 void f(void Function({int y}) x) {}
