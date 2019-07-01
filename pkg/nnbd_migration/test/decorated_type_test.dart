a40d903c9eda01b8129c55270621bd42b6832b12
Paul Berry <paulberry@google.com>
2019-07-01 14:04:55 +0200

    Fix unused imports

--- pkg/nnbd_migration/test/decorated_type_test.dart
+++ pkg/nnbd_migration/test/decorated_type_test.dart
@@ -2,15 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/dart/ast/ast.dart';
-import 'package:nnbd_migration/src/decorated_class_hierarchy.dart';
 import 'package:nnbd_migration/src/decorated_type.dart';
 import 'package:nnbd_migration/src/nullability_node.dart';
 import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'abstract_single_unit.dart';
-import 'migration_visitor_test_base.dart';
 
 main() {
   defineReflectiveSuite(() {
