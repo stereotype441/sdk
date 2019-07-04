61a17e7d4041790f84a524f2eb4cf9464fb34144
Paul Berry <paulberry@google.com>
2019-07-04 05:51:51 +0200

    WIP

--- pkg/analyzer/test/src/dart/resolution/flow_analysis_dsl.dart
+++ pkg/analyzer/test/src/dart/resolution/flow_analysis_dsl.dart
@@ -1,119 +1,52 @@
-class Get implements Expression {
-  final Variable variable;
-
-  Get(this.variable);
-}
-class Variable {}
-class TypeAnnotation {
-  final String class_;
-
-  TypeAnnotation(this.class_);
-}
-class Param implements Variable {
-  final TypeAnnotation type;
-  final String name;
-
-  Param(this.type, this.name);
-}
-class Declaration {}
-class Unit {
-  final List<Declaration> declarations;
-
-  Unit(this.declarations);
-}
-class Statement {}
-class Constructor implements Declaration {
-  final String name;
-  final List<Param> parameters;
-  Statement body;
-
-  Constructor(this.name, this.parameters, [this.body]);
-}
-class Func implements Declaration, Statement {
-  final TypeAnnotation returnType;
-  final String name;
-  final List<Param> parameters;
-  Statement body;
-
-  Func(this.returnType, this.name, this.parameters, [this.body]);
-}
-class Method implements Declaration {
-  final TypeAnnotation returnType;
-  final String name;
-  final List<Param> parameters;
-  Statement body;
-
-  Method(this.returnType, this.name, this.parameters, [this.body]);
-}
-class StringLiteral implements Expression {
-  final String value;
-
-  StringLiteral(this.value);
-}
-class Block implements Statement {
-  final List<Statement> statements;
-
-  Block(this.statements);
-}
-class Expression implements Statement {}
-class NullLiteral implements Expression {}
-class Return implements Statement {
-  final Expression value;
-
-  Return([this.value]);
-}
-class Parens implements Expression {
-  final Expression contents;
-
-  Parens(this.contents);
-}
-class Throw implements Expression {
-  final Expression exception;
-
-  Throw(this.exception);
-}
-class IfNull implements Expression {
-  final Expression left;
-  final Expression right;
-
-  IfNull(this.left, this.right);
-}
-class NotEq implements Expression {
-  final Expression left;
-  final Expression right;
-
-  NotEq(this.left, this.right);
-}
-class Eq implements Expression {
-  final Expression left;
-  final Expression right;
-
-  Eq(this.left, this.right);
-}
-class Class implements Declaration {
-  final String name;
-  final List<Declaration> members;
-
-  Class(this.name, this.members);
-}
-class Or implements Expression {
-  final Expression left;
-  final Expression right;
-
-  Or(this.left, this.right);
-}
-class PropertyGet implements Expression {
-  final Expression target;
-  final String propertyName;
-
-  PropertyGet(this.target, this.propertyName);
-}
 class And implements Expression {
   final Expression left;
   final Expression right;
 
   And(this.left, this.right);
 }
+
+class Block implements Statement {
+  final List<Statement> statements;
+
+  Block(this.statements);
+}
+
+class Bool implements Expression {
+  final bool value;
+
+  Bool(this.value);
+}
+
+class Break implements Statement {}
+
+class Case {
+  final List<String> labels;
+  final Expression value;
+  final List<Statement> body;
+
+  Case(this.labels, this.value, this.body);
+}
+
+class Catch {
+  final CatchVariable exception;
+  final Statement body;
+
+  Catch(this.exception, this.body);
+}
+
+class CatchVariable {
+  final String name;
+
+  CatchVariable(this.name);
+}
+
+class Class implements Declaration {
+  final String name;
+  final List<Declaration> members;
+
+  Class(this.name, this.members);
+}
+
 class Conditional implements Expression {
   final Expression condition;
   final Expression thenExpression;
@@ -121,18 +54,53 @@ class Conditional implements Expression {
 
   Conditional(this.condition, this.thenExpression, this.elseExpression);
 }
-class If implements Statement {
+
+class Constructor implements Declaration {
+  final String name;
+  final List<Param> parameters;
+  Statement body;
+
+  Constructor(this.name, this.parameters, [this.body]);
+}
+
+class Continue implements Statement {}
+
+class Declaration {}
+
+class Do implements Expression {
+  final Statement body;
   final Expression condition;
-  final Statement thenStatement;
-  final Statement elseStatement;
 
-  If(this.condition, this.thenStatement, [this.elseStatement]);
+  Do(this.body, this.condition);
 }
-class Locals implements Statement {
-  final List<Local> variables;
 
-  Locals(this.variables);
+class Eq implements Expression {
+  final Expression left;
+  final Expression right;
+
+  Eq(this.left, this.right);
 }
+
+class Expression implements Statement {}
+
+class ForEachDeclared implements Statement {
+  final List<ForEachVariable> variables;
+  final Expression iterable;
+  final Statement body;
+
+  ForEachDeclared(this.variables, this.iterable, this.body);
+}
+
+class ForEachIdentifier implements Statement {
+  final Variable variable;
+  final Expression iterable;
+  final Statement body;
+
+  ForEachIdentifier(this.variable, this.iterable, this.body);
+}
+
+class ForEachVariable {}
+
 class ForExpr implements Statement {
   final Expression initializer;
   final Expression condition;
@@ -141,78 +109,162 @@ class ForExpr implements Statement {
 
   ForExpr(this.initializer, this.condition, this.updaters, this.body);
 }
-class ForEachIdentifier implements Statement {
+
+class Func implements Declaration, Statement {
+  final TypeAnnotation returnType;
+  final String name;
+  final List<Param> parameters;
+  Statement body;
+
+  Func(this.returnType, this.name, this.parameters, [this.body]);
+}
+
+class Get implements Expression {
   final Variable variable;
-  final Expression iterable;
-  final Statement body;
 
-  ForEachIdentifier(this.variable, this.iterable, this.body);
+  Get(this.variable);
 }
-class Local implements Variable {
-  final String name;
 
-  Local(this.name);
-}
-class ListLiteral implements Expression {
-  final TypeAnnotation type;
-  final List<Expression> values;
+class Gt implements Expression {
+  final Expression left;
+  final Expression right;
 
-  ListLiteral(this.type, this.values);
+  Gt(this.left, this.right);
 }
-class CatchVariable {
-  final String name;
 
-  CatchVariable(this.name);
-}
-class Catch {
-  final CatchVariable exception;
-  final Statement body;
-
-  Catch(this.exception, this.body);
-}
-class Case {
-  final List<String> labels;
-  final Expression value;
-  final List<Statement> body;
-
-  Case(this.labels, this.value, this.body);
-}
-class Switch implements Statement {
-  final Expression value;
-  final List<Case> cases;
-
-  Switch(this.value, this.cases);
-}
-class Do implements Expression {
-  final Statement body;
+class If implements Statement {
   final Expression condition;
+  final Statement thenStatement;
+  final Statement elseStatement;
 
-  Do(this.body, this.condition);
+  If(this.condition, this.thenStatement, [this.elseStatement]);
 }
-class While implements Expression {
-  final Expression condition;
-  final Statement body;
 
-  While(this.condition, this.body);
-}
-class Bool implements Expression {
-  final bool value;
+class IfNull implements Expression {
+  final Expression left;
+  final Expression right;
 
-  Bool(this.value);
+  IfNull(this.left, this.right);
 }
+
 class Int implements Expression {
   final int value;
 
   Int(this.value);
 }
-class Break implements Statement {}
-class Continue implements Statement {}
+
 class Is implements Expression {
   final Expression expression;
   final TypeAnnotation type;
 
   Is(this.expression, this.type);
 }
+
+class ListLiteral implements Expression {
+  final TypeAnnotation type;
+  final List<Expression> values;
+
+  ListLiteral(this.type, this.values);
+}
+
+class Local implements Variable {
+  final String name;
+
+  Local(this.name);
+}
+
+class Locals implements Statement {
+  final List<Local> variables;
+
+  Locals(this.variables);
+}
+
+class Method implements Declaration {
+  final TypeAnnotation returnType;
+  final String name;
+  final List<Param> parameters;
+  Statement body;
+
+  Method(this.returnType, this.name, this.parameters, [this.body]);
+}
+
+class NotEq implements Expression {
+  final Expression left;
+  final Expression right;
+
+  NotEq(this.left, this.right);
+}
+
+class NullLiteral implements Expression {}
+
+class Or implements Expression {
+  final Expression left;
+  final Expression right;
+
+  Or(this.left, this.right);
+}
+
+class Param implements Variable {
+  final TypeAnnotation type;
+  final String name;
+
+  Param(this.type, this.name);
+}
+
+class Parens implements Expression {
+  final Expression contents;
+
+  Parens(this.contents);
+}
+
+class PropertyGet implements Expression {
+  final Expression target;
+  final String propertyName;
+
+  PropertyGet(this.target, this.propertyName);
+}
+
+class Return implements Statement {
+  final Expression value;
+
+  Return([this.value]);
+}
+
+class Set implements Expression {
+  final Variable variable;
+  final Expression value;
+
+  Set(this.variable, this.value);
+}
+
+class Statement {}
+
+class StaticCall implements Expression {
+  final Func target;
+  final List<Expression> arguments;
+
+  StaticCall(this.target, this.arguments);
+}
+
+class StringLiteral implements Expression {
+  final String value;
+
+  StringLiteral(this.value);
+}
+
+class Switch implements Statement {
+  final Expression value;
+  final List<Case> cases;
+
+  Switch(this.value, this.cases);
+}
+
+class Throw implements Expression {
+  final Expression exception;
+
+  Throw(this.exception);
+}
+
 class Try implements Expression {
   final Statement body;
   final List<Catch> catches;
@@ -220,15 +272,24 @@ class Try implements Expression {
 
   Try(this.body, this.catches, [this.finallyClause]);
 }
-class StaticCall implements Expression {
-  final Func target;
-  final List<Expression> arguments;
 
-  StaticCall(this.target, this.arguments);
+class TypeAnnotation {
+  final String class_;
+
+  TypeAnnotation(this.class_);
 }
-class Set implements Expression {
-  final Variable variable;
-  final Expression value;
 
-  Set(this.variable, this.value);
+class Unit {
+  final List<Declaration> declarations;
+
+  Unit(this.declarations);
+}
+
+class Variable {}
+
+class While implements Expression {
+  final Expression condition;
+  final Statement body;
+
+  While(this.condition, this.body);
 }
