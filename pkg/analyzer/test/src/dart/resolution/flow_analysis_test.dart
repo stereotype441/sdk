2d0902ef81119eeda7d6727658de4c5b538498fa
Paul Berry <paulberry@google.com>
2019-07-05 10:06:48 +0200

    Fix

--- pkg/analyzer/test/src/dart/resolution/flow_analysis_test.dart
+++ pkg/analyzer/test/src/dart/resolution/flow_analysis_test.dart
@@ -364,7 +364,8 @@ void f() {
 
   test_exit_beforeSplitStatement() async {
     await trackCode(r'''
-void f(bool b, int i) /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f(bool b, int i) {
   return;
   /*statement: unreachable*/ Object _;
   /*statement: unreachable*/ do {} while (b);
@@ -380,7 +381,8 @@ void f(bool b, int i) /*functionBody: doesNotComplete*/ {
 
   test_for_condition_true() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   for (; true;) {
     1;
   }
@@ -391,7 +393,8 @@ void f() /*functionBody: doesNotComplete*/ {
 
   test_for_condition_true_implicit() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   for (;;) {
     1;
   }
@@ -415,7 +418,8 @@ void f() {
 
   test_functionBody_hasReturn() async {
     await trackCode(r'''
-int f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+int f() {
   return 42;
 }
 ''');
@@ -456,7 +460,8 @@ void f() {
 
   test_if_true_return() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   1;
   if (true) {
     return;
@@ -573,7 +578,8 @@ void f() {
 
   test_tryCatchFinally_return_bodyCatch() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   try {
     1;
     return;
@@ -606,7 +612,8 @@ void f() {
 
   test_tryFinally_return_body() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   try {
     1;
     return;
@@ -631,7 +638,8 @@ void f() {
 
   test_while_true() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   while (true) {
     1;
   }
@@ -669,7 +677,8 @@ void f(bool b) {
 
   test_while_true_continue() async {
     await trackCode(r'''
-void f() /*functionBody: doesNotComplete*/ {
+/*member: f:doesNotComplete*/
+void f() {
   while (true) {
     1;
     continue;
