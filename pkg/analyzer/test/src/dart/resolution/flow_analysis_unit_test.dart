5adfdaaa8d5fd2b195f6c06ca081d4d3cbaf0e7a
Paul Berry <paulberry@google.com>
2019-07-19 16:34:04 +0200

    Fix

--- pkg/analyzer/test/src/dart/resolution/flow_analysis_unit_test.dart
+++ pkg/analyzer/test/src/dart/resolution/flow_analysis_unit_test.dart
@@ -21,72 +21,78 @@ import 'driver_resolution.dart';
 
 main() {
   group('Flow analysis unit tests', () {
-    test('foo', () {
-      var nodeOperations = _NodeOperations();
-      var typeOperations = _TypeOperations();
-      var functionBodyAccess = _FunctionBodyAccess();
-      var flow = FlowAnalysis<_Statement, _Expression, _Variable, _Type>(
-          nodeOperations, typeOperations, functionBodyAccess);
-      var x = _Variable();
+    test('conditionNotEqNull promotes true branch', () {
+      var flow = _Harness().flow;
+      var x = _Var('int?');
       flow.add(x, assigned: true);
-      var xNotEqNull = _Expression();
-      flow.conditionNotEqNull(xNotEqNull, x);
-      flow.ifStatement_thenBegin(xNotEqNull);
-      flow.handleExit();
-      flow.ifStatement_end(false);
-      var xPromotedType = flow.promotedType(x);
-      fail('$xPromotedType');
+      var expr = _Expression();
+      flow.conditionNotEqNull(expr, x);
+      flow.ifStatement_thenBegin(expr);
+      expect(flow.promotedType(x), 'int');
+      flow.ifStatement_elseBegin();
+      expect(flow.promotedType(x), isNull);
+      flow.ifStatement_end(true);
       flow.verifyStackEmpty();
     });
   });
 }
 
-class _Variable {}
-
 class _Expression {}
 
-class _FunctionBodyAccess implements FunctionBodyAccess<_Variable> {
+class _Harness
+    implements
+        NodeOperations<_Expression>,
+        TypeOperations<_Var, String>,
+        FunctionBodyAccess<_Var> {
+  FlowAnalysis<_Statement, _Expression, _Var, String> flow;
+
+  _Harness() {
+    flow =
+        FlowAnalysis<_Statement, _Expression, _Var, String>(this, this, this);
+  }
+
   @override
-  bool isPotentiallyMutatedInClosure(_Variable variable) {
+  bool isLocalVariable(_Var variable) {
+    throw UnimplementedError('TODO(paulberry)');
+  }
+
+  @override
+  bool isPotentiallyMutatedInClosure(_Var variable) {
     // TODO(paulberry): make tests where this returns true
     return false;
   }
 
   @override
-  bool isPotentiallyMutatedInScope(_Variable variable) {
+  bool isPotentiallyMutatedInScope(_Var variable) {
     throw UnimplementedError('TODO(paulberry)');
   }
-}
 
-class _NodeOperations implements NodeOperations<_Expression> {
+  @override
+  bool isSubtypeOf(String leftType, String rightType) {
+    throw UnimplementedError('TODO(paulberry)');
+  }
+  @override
+  String promoteToNonNull(String type) {
+    if (type.endsWith('?')) return type.substring(0, type.length - 1);
+    return type;
+  }
+
   @override
   _Expression unwrapParenthesized(_Expression node) {
-    throw UnimplementedError('TODO(paulberry)');
+    // TODO(paulberry): test cases where this matters
+    return node;
+  }
+
+  @override
+  String variableType(_Var variable) {
+    return variable.type;
   }
 }
 
 class _Statement {}
 
-class _Type {}
+class _Var {
+  final String type;
 
-class _TypeOperations implements TypeOperations<_Variable, _Type> {
-  @override
-  _Type variableType(_Variable variable) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  @override
-  bool isLocalVariable(_Variable variable) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  @override
-  bool isSubtypeOf(_Type leftType, _Type rightType) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  @override
-  _Type promoteToNonNull(_Type type) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
+  _Var(this.type);
 }
