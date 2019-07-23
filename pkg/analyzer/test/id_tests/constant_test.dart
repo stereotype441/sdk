68eff63deab21573b8c39ce424f8dad9bb001462
Paul Berry <paulberry@google.com>
2019-07-23 05:21:38 -0700

    WIP

--- pkg/analyzer/test/id_tests/constant_test.dart
+++ pkg/analyzer/test/id_tests/constant_test.dart
@@ -3,66 +3,45 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:io';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/src/util/ast_data_extractor.dart';
 import 'package:front_end/src/testing/id_testing.dart';
-import 'package:test/test.dart';
+import 'package:front_end/src/testing/id.dart' show ActualData, Id;
+import '../util/id_testing_helper.dart';
 
-main(List<String> args) {
-  test('Constant ID tests', () async {
-    Directory dataDir = new Directory.fromUri(Platform.script
-        .resolve('../../../../pkg/front_end/test/constants/data'));
-    await checkTests(dataDir, new ConstantDataComputer(),
-        args: args,
-        testedConfigs: [sharedConfig],
-        supportedMarkers: sharedMarkers);
-  });
+main(List<String> args) async {
+  Directory dataDir = new Directory.fromUri(Platform.script.resolve('data'));
+  await runTests(dataDir,
+      args: args,
+      supportedMarkers: sharedMarkers,
+      createUriForFileName: createUriForFileName,
+      onFailure: onFailure,
+      runTest: runTestFor(
+          const ConstantsDataComputer(), [analyzerConstantUpdate2018Config]));
 }
 
-class ConstantDataComputer extends DataComputer<String> {
-  ir.TypeEnvironment _typeEnvironment;
-
-  ir.TypeEnvironment getTypeEnvironment(KernelToElementMapImpl elementMap) {
-    if (_typeEnvironment == null) {
-      ir.Component component = elementMap.env.mainComponent;
-      _typeEnvironment = new ir.TypeEnvironment(
-          new ir.CoreTypes(component), new ir.ClassHierarchy(component));
-    }
-    return _typeEnvironment;
-  }
-
-  /// Compute type inference data for [member] from kernel based inference.
-  ///
-  /// Fills [actualMap] with the data.
-  @override
-  void computeMemberData(Compiler compiler, MemberEntity member,
-      Map<Id, ActualData<String>> actualMap,
-      {bool verbose: false}) {
-    KernelFrontendStrategy frontendStrategy = compiler.frontendStrategy;
-    KernelToElementMapImpl elementMap = frontendStrategy.elementMap;
-    ir.Member node = elementMap.getMemberNode(member);
-    new ConstantDataExtractor(compiler.reporter, actualMap, elementMap)
-        .run(node);
-  }
+class ConstantsDataComputer extends DataComputer<String> {
+  const ConstantsDataComputer();
 
   @override
-  bool get testFrontend => true;
+  void computeUnitData(CompilationUnit unit,
+      Map<Id, ActualData<String>> actualMap) {
+    ConstantsDataExtractor(unit.declaredElement.source.uri, actualMap)
+        .run(unit);
+  }
 
   @override
   DataInterpreter<String> get dataValidator => const StringDataInterpreter();
 }
 
-/// IR visitor for computing inference data for a member.
-class ConstantDataExtractor extends IrDataExtractor<String> {
-  final KernelToElementMapImpl elementMap;
-
-  ConstantDataExtractor(DiagnosticReporter reporter,
-      Map<Id, ActualData<String>> actualMap, this.elementMap)
-      : super(reporter, actualMap);
+class ConstantsDataExtractor extends AstDataExtractor<String> {
+  ConstantsDataExtractor(
+      Uri uri, Map<Id, ActualData<String>> actualMap)
+      : super(uri, actualMap);
 
   @override
-  String computeNodeValue(Id id, ir.TreeNode node) {
-    if (node is ir.ConstantExpression) {
-      return constantToText(elementMap.getConstantValue(node));
-    }
+  String computeNodeValue(Id id, AstNode node) {
+    // TODO(paulberry): figure out what to do here.
     return null;
   }
 }
--- pkg/analyzer/test/util/id_testing_helper.dart
+++ pkg/analyzer/test/util/id_testing_helper.dart
@@ -5,6 +5,7 @@
 // TODO(paulberry,johnniwinther): Use the code for extraction of test data from
 // annotated code from CFE.
 
+import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:analyzer/dart/ast/ast.dart' hide Annotation;
@@ -12,30 +13,6 @@ import 'package:front_end/src/testing/annotated_code_helper.dart';
 import 'package:front_end/src/testing/id.dart'
     show ActualData, Id, IdValue, MemberId, NodeId;
 import 'package:front_end/src/testing/id_testing.dart';
-
-Future<bool> checkTests<T>(
-    String rawCode,
-    Future<ResolvedUnitResult> resultComputer(String rawCode),
-    DataComputer<T> dataComputer) async {
-  AnnotatedCode code =
-      new AnnotatedCode.fromText(rawCode, commentStart, commentEnd);
-  var result = await resultComputer(code.sourceCode);
-  var uri = result.libraryElement.source.uri;
-  var marker = 'normal';
-  Map<String, MemberAnnotations<IdValue>> expectedMaps = {
-    marker: new MemberAnnotations<IdValue>(),
-  };
-  computeExpectedMap(uri, code, expectedMaps, onFailure: onFailure);
-  MemberAnnotations<IdValue> annotations = expectedMaps[marker];
-  Map<Id, ActualData<T>> actualMap = {};
-  dataComputer.computeUnitData(result.unit, actualMap);
-  Map<Uri, AnnotatedCode> codeMap = {uri: code};
-  var compiledData =
-      AnalyzerCompiledData<T>(codeMap, uri, {uri: actualMap}, {});
-  return await checkCode(marker, uri, codeMap, annotations, compiledData,
-      dataComputer.dataValidator,
-      onFailure: onFailure);
-}
 
 class AnalyzerCompiledData<T> extends CompiledData<T> {
   // TODO(johnniwinther,paulberry): Maybe this should have access to the
@@ -83,6 +60,229 @@ class AnalyzerCompiledData<T> extends CompiledData<T> {
 void onFailure(String message) {
   throw StateError(message);
 }
+
+class TestConfig {
+  final String marker;
+  final String name;
+  final FeatureSet featureSet;
+
+  TestConfig(this.marker, this.name, {FeatureSet featureSet}) : featureSet = featureSet ?? FeatureSet.fromEnableFlags([]);
+}
+
+/// Creates a test runner for [dataComputer] on [testedConfigs].
+RunTestFunction runTestFor<T>(
+    DataComputer<T> dataComputer, List<TestConfig> testedConfigs) {
+  return (TestData testData,
+      {bool testAfterFailures, bool verbose, bool printCode}) {
+    return runTest(testData, dataComputer, testedConfigs,
+        testAfterFailures: testAfterFailures,
+        verbose: verbose,
+        printCode: printCode,
+        onFailure: onFailure);
+  };
+}
+
+/// Runs [dataComputer] on [testData] for all [testedConfigs].
+///
+/// Returns `true` if an error was encountered.
+Future<bool> runTest<T>(TestData testData, DataComputer<T> dataComputer,
+    List<TestConfig> testedConfigs,
+    {bool testAfterFailures,
+      bool verbose,
+      bool printCode,
+      bool forUserLibrariesOnly: true,
+      Iterable<Id> globalIds: const <Id>[],
+      void onFailure(String message)}) async {
+  bool hasFailures = false;
+  for (TestConfig config in testedConfigs) {
+    if (await runTestForConfig(testData, dataComputer, config,
+        fatalErrors: !testAfterFailures,
+        onFailure: onFailure,
+        verbose: verbose,
+        printCode: printCode)) {
+      hasFailures = true;
+    }
+  }
+  return hasFailures;
+}
+
+Future<bool> checkTests<T>(
+    String rawCode,
+    Future<ResolvedUnitResult> resultComputer(String rawCode),
+    DataComputer<T> dataComputer) async {
+  AnnotatedCode code =
+  new AnnotatedCode.fromText(rawCode, commentStart, commentEnd);
+  var result = await resultComputer(code.sourceCode);
+  var uri = result.libraryElement.source.uri;
+  var marker = 'normal';
+  Map<String, MemberAnnotations<IdValue>> expectedMaps = {
+    marker: new MemberAnnotations<IdValue>(),
+  };
+  computeExpectedMap(uri, code, expectedMaps, onFailure: onFailure);
+  MemberAnnotations<IdValue> annotations = expectedMaps[marker];
+  Map<Id, ActualData<T>> actualMap = {};
+  dataComputer.computeUnitData(result.unit, actualMap);
+  Map<Uri, AnnotatedCode> codeMap = {uri: code};
+  var compiledData =
+  AnalyzerCompiledData<T>(codeMap, uri, {uri: actualMap}, {});
+  return await checkCode(marker, uri, codeMap, annotations, compiledData,
+      dataComputer.dataValidator,
+      onFailure: onFailure);
+}
+
+/// Runs [dataComputer] on [testData] for [config].
+///
+/// Returns `true` if an error was encountered.
+Future<bool> runTestForConfig<T>(
+    TestData testData, DataComputer<T> dataComputer, TestConfig config,
+    {bool fatalErrors,
+      bool verbose,
+      bool printCode,
+      bool forUserLibrariesOnly: true,
+      Iterable<Id> globalIds: const <Id>[],
+      void onFailure(String message)}) async {
+  MemberAnnotations<IdValue> memberAnnotations =
+  testData.expectedMaps[config.marker];
+  Iterable<Id> globalIds = memberAnnotations.globalData.keys;
+  CompilerOptions options = new CompilerOptions();
+  options.debugDump = printCode;
+  options.experimentalFlags.addAll(config.experimentalFlags);
+  CompilerResult compilerResult = await compileScript(
+      testData.memorySourceFiles,
+      options: options,
+      retainDataForTesting: true);
+  Component component = compilerResult.component;
+  Map<Uri, Map<Id, ActualData<T>>> actualMaps = <Uri, Map<Id, ActualData<T>>>{};
+  Map<Id, ActualData<T>> globalData = <Id, ActualData<T>>{};
+
+  Map<Id, ActualData<T>> actualMapFor(TreeNode node) {
+    Uri uri = node is Library ? node.fileUri : node.location.file;
+    return actualMaps.putIfAbsent(uri, () => <Id, ActualData<T>>{});
+  }
+
+  void processMember(Member member, Map<Id, ActualData<T>> actualMap) {
+    if (member.enclosingClass != null) {
+      if (member.enclosingClass.isEnum) {
+        if (member is Constructor ||
+            member.isInstanceMember ||
+            member.name == 'values') {
+          return;
+        }
+      }
+      if (member is Constructor && member.enclosingClass.isMixinApplication) {
+        return;
+      }
+    }
+    dataComputer.computeMemberData(compilerResult, member, actualMap,
+        verbose: verbose);
+  }
+
+  void processClass(Class cls, Map<Id, ActualData<T>> actualMap) {
+    dataComputer.computeClassData(compilerResult, cls, actualMap,
+        verbose: verbose);
+  }
+
+  bool excludeLibrary(Library library) {
+    return forUserLibrariesOnly &&
+        (library.importUri.scheme == 'dart' ||
+            library.importUri.scheme == 'package');
+  }
+
+  for (Library library in component.libraries) {
+    if (excludeLibrary(library)) continue;
+    dataComputer.computeLibraryData(
+        compilerResult, library, actualMapFor(library));
+    for (Class cls in library.classes) {
+      processClass(cls, actualMapFor(cls));
+      for (Member member in cls.members) {
+        processMember(member, actualMapFor(member));
+      }
+    }
+    for (Member member in library.members) {
+      processMember(member, actualMapFor(member));
+    }
+  }
+
+  List<Uri> globalLibraries = <Uri>[
+    Uri.parse('dart:core'),
+    Uri.parse('dart:collection'),
+    Uri.parse('dart:async'),
+  ];
+
+  Class getGlobalClass(String className) {
+    Class cls;
+    for (Uri uri in globalLibraries) {
+      Library library = lookupLibrary(component, uri);
+      if (library != null) {
+        cls ??= lookupClass(library, className);
+      }
+    }
+    if (cls == null) {
+      throw "Global class '$className' not found in the global "
+          "libraries: ${globalLibraries.join(', ')}";
+    }
+    return cls;
+  }
+
+  Member getGlobalMember(String memberName) {
+    Member member;
+    for (Uri uri in globalLibraries) {
+      Library library = lookupLibrary(component, uri);
+      if (library != null) {
+        member ??= lookupLibraryMember(library, memberName);
+      }
+    }
+    if (member == null) {
+      throw "Global member '$memberName' not found in the global "
+          "libraries: ${globalLibraries.join(', ')}";
+    }
+    return member;
+  }
+
+  for (Id id in globalIds) {
+    if (id is MemberId) {
+      Member member;
+      if (id.className != null) {
+        Class cls = getGlobalClass(id.className);
+        member = lookupClassMember(cls, id.memberName);
+        if (member == null) {
+          throw "Global member '${id.memberName}' not found in class $cls.";
+        }
+      } else {
+        member = getGlobalMember(id.memberName);
+      }
+      processMember(member, globalData);
+    } else if (id is ClassId) {
+      Class cls = getGlobalClass(id.className);
+      processClass(cls, globalData);
+    } else {
+      throw new UnsupportedError("Unexpected global id: $id");
+    }
+  }
+
+  CfeCompiledData compiledData = new CfeCompiledData<T>(
+      compilerResult, testData.testFileUri, actualMaps, globalData);
+
+  return checkCode(config.name, testData.testFileUri, testData.code,
+      memberAnnotations, compiledData, dataComputer.dataValidator,
+      fatalErrors: fatalErrors, onFailure: onFailure);
+}
+
+/// Test configuration used for testing CFE with constant evaluation.
+final TestConfig analyzerConstantUpdate2018Config = TestConfig(
+    analyzerMarker, 'analyzer with constant-update-2018',
+    featureSet: FeatureSet.forTesting(sdkVersion: '2.2.2', additionalFeatures: [Feature.constant_update_2018]));
+
+/// Creates the testing URI used for [fileName] in annotated tests.
+Uri createUriForFileName(String fileName, {bool isLib}) => _toTestUri(fileName);
+
+/// A fake absolute directory used as the root of a memory-file system in ID
+/// tests.
+Uri _defaultDir = Uri.parse('file:///a/b/c/');
+
+/// Convert relative file paths into an absolute Uri as expected by the test
+/// helpers.
+Uri _toTestUri(String relativePath) => _defaultDir.resolve(relativePath);
 
 abstract class DataComputer<T> {
   const DataComputer();
