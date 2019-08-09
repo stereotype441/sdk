d1870afc83f4d54e29cd93a2abba76c5d538fcfb
Paul Berry <paulberry@google.com>
2019-08-09 09:20:16 -0700

    Cleanup

--- pkg/nnbd_migration/lib/src/already_migrated_code_decorator.dart
+++ pkg/nnbd_migration/lib/src/already_migrated_code_decorator.dart
@@ -7,17 +7,26 @@ import 'package:analyzer/src/dart/element/type.dart';
 import 'package:nnbd_migration/src/decorated_type.dart';
 import 'package:nnbd_migration/src/nullability_node.dart';
 
+/// This class transforms ordinary [DartType]s into their corresponding
+/// [DecoratedType]s, assuming the [DartType]s come from code that has already
+/// been migrated to NNBD.
 class AlreadyMigratedCodeDecorator {
-  final NullabilityGraph graph;
+  final NullabilityGraph _graph;
 
-  AlreadyMigratedCodeDecorator(this.graph);
+  AlreadyMigratedCodeDecorator(this._graph);
 
+  /// Transforms [type], which should have come from code that has already been
+  /// migrated to NNBD, into the corresponding [DecoratedType].
   DecoratedType decorate(DartType type) {
     if (type.isVoid || type.isDynamic) {
-      return DecoratedType(type, graph.always);
+      return DecoratedType(type, _graph.always);
     }
-    assert((type as TypeImpl).nullabilitySuffix ==
-        NullabilitySuffix.star); // TODO(paulberry)
+    // Currently, all types passed to this method have nullability suffix `star`
+    // because (a) we don't yet have a migrated SDK, and (b) we haven't added
+    // support to the migrator for analyzing packages that have already been
+    // migrated with NNBD enabled.
+    // TODO(paulberry): fix this assertion when things change.
+    assert((type as TypeImpl).nullabilitySuffix == NullabilitySuffix.star);
     if (type is FunctionType) {
       var positionalParameters = <DecoratedType>[];
       var namedParameters = <String, DecoratedType>{};
@@ -28,7 +37,7 @@ class AlreadyMigratedCodeDecorator {
           namedParameters[parameter.name] = decorate(parameter.type);
         }
       }
-      return DecoratedType(type, graph.never,
+      return DecoratedType(type, _graph.never,
           returnType: decorate(type.returnType),
           namedParameters: namedParameters,
           positionalParameters: positionalParameters);
@@ -37,9 +46,9 @@ class AlreadyMigratedCodeDecorator {
         // TODO(paulberry)
         throw UnimplementedError('Decorating ${type.displayName}');
       }
-      return DecoratedType(type, graph.never);
+      return DecoratedType(type, _graph.never);
     } else if (type is TypeParameterType) {
-      return DecoratedType(type, graph.never);
+      return DecoratedType(type, _graph.never);
     } else {
       throw type.runtimeType; // TODO(paulberry)
     }
