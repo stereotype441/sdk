ba153c9cdcdc8d75616181204b10450a8ad4cec9
Paul Berry <paulberry@google.com>
2019-08-21 08:15:14 -0700

    Fix

--- pkg/nnbd_migration/test/node_builder_test.dart
+++ pkg/nnbd_migration/test/node_builder_test.dart
@@ -782,6 +782,14 @@ abstract class C {
     expect(decoratedFReturnReturnType.node, same(always));
   }
 
+  test_genericFunctionType_formal_bounds() async {
+    await analyze('''
+void f(T Function<T extends num>() x) {}
+''');
+    var decoratedType = decoratedGenericFunctionTypeAnnotation('T Function');
+    expect(decoratedType.typeFormalBounds[0].type.toString(), 'num');
+  }
+
   test_genericFunctionType_formals() async {
     await analyze('''
 void f(T Function<T, U>(U) x) {}
@@ -790,7 +798,7 @@ void f(T Function<T, U>(U) x) {}
     expect(decoratedFunctionType('f').positionalParameters[0],
         same(decoratedType));
     expect(decoratedType.node, TypeMatcher<NullabilityNodeMutable>());
-    expect(decoratedType.type.toString(), 'T Function<T, U>(U)');
+    expect(decoratedType.type.toString(), 'T Function<T,U>(U)');
     expect(decoratedType.typeFormals, hasLength(2));
     var t = decoratedType.typeFormals[0];
     var u = decoratedType.typeFormals[1];
