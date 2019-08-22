dbd5ac68ed6d9400f35035e997524c5c83d44cbe
Paul Berry <paulberry@google.com>
2019-08-22 15:40:14 -0700

    WIP

--- pkg/front_end/test/fasta/flow_analysis/assigned_variables_test.dart
+++ pkg/front_end/test/fasta/flow_analysis/assigned_variables_test.dart
@@ -5,31 +5,100 @@
 import 'package:front_end/src/fasta/flow_analysis/flow_analysis.dart';
 import 'package:test/test.dart';
 
+main() {
+  test('assignedInClosureAnywhere records assignments in closures', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    var v2 = _Variable('v2');
+    var v3 = _Variable('v3');
+    av.write(v1);
+    av.beginClosure();
+    av.write(v2);
+    av.endClosure(_Node());
+    av.write(v3);
+    expect(av.assignedInClosureAnywhere, {v2});
+  });
+
+  test('assigned ignores assignments outside the node', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    var v2 = _Variable('v2');
+    av.write(v1);
+    av.beginStatementOrElement();
+    var node = _Node();
+    av.endStatementOrElement(node);
+    av.write(v2);
+    expect(av.assigned(node), isEmpty);
+  });
+
+  test('assigned records assignments inside the node', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    av.beginStatementOrElement();
+    av.write(v1);
+    var node = _Node();
+    av.endStatementOrElement(node);
+    expect(av.assigned(node), {v1});
+  });
+
+  test('assigned records assignments in a nested node', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    av.beginStatementOrElement();
+    av.beginStatementOrElement();
+    av.write(v1);
+    av.endStatementOrElement(_Node());
+    var node = _Node();
+    av.endStatementOrElement(node);
+    expect(av.assigned(node), {v1});
+  });
+
+  test('assigned records assignments in a closure', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    av.beginClosure();
+    av.write(v1);
+    var node = _Node();
+    av.endClosure(node);
+    expect(av.assigned(node), {v1});
+  });
+
+  test('assignedInClosure ignores assignments in non-nested closures', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    var v2 = _Variable('v2');
+    av.beginClosure();
+    av.write(v1);
+    av.endClosure(_Node());
+    av.beginStatementOrElement();
+    var node = _Node();
+    av.endStatementOrElement(node);
+    av.beginClosure();
+    av.write(v2);
+    av.endClosure(_Node());
+    expect(av.assignedInClosure(node), isEmpty);
+  });
+
+  test('assignedInClosure records assignments in nested closures', () {
+    var av = AssignedVariables<_Node, _Variable>();
+    var v1 = _Variable('v1');
+    av.beginStatementOrElement();
+    av.beginClosure();
+    av.write(v1);
+    av.endClosure(_Node());
+    var node = _Node();
+    av.endStatementOrElement(node);
+    expect(av.assigned(node), {v1});
+  });
+}
+
 class _Node {}
 
 class _Variable {
   final String name;
 
   _Variable(this.name);
-}
 
-class _Harness {
-  final av = AssignedVariables<_Node, _Variable>();
-}
-
-main() {
-  test('assignedInClosureAnywhere records assignments in closures', () {
-    var h = _Harness();
-    var v1 = _Variable('v1');
-    var v2 = _Variable('v2');
-    var v3 = _Variable('v3');
-    h.av.write(v1);
-    h.av.beginClosure();
-    h.av.write(v2);
-    h.av.endClosure(_Node());
-    h.av.write(v3);
-    expect(h.av.assignedInClosureAnywhere, {v2});
-  });
-
-  test('assignedInClosure')
+  @override
+  String toString() => name;
 }
