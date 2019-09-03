4954506cc5e7258b897206326c7761491584e2b4
Paul Berry <paulberry@google.com>
2019-09-03 05:51:56 -0700

    Test still fails

--- pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
+++ pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
@@ -1,14 +1,3 @@
-main() {
-  for (int i = 0; i < 20; i++) {
-    var timer = new Stopwatch();
-    timer.start();
-    List<int> primes = findPrimes(1000000);
-    timer.stop();
-    print('[$i] ${timer.elapsedMilliseconds} ms.');
-    primes; // print?
-  }
-}
-
 List<int> findPrimes(int last) {
   List<int> primes = [];
   for (int x = 2; x < last; x++) {
