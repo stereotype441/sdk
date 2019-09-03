85faf4e1372509106964ff920a05bc8614c7ba4b
Paul Berry <paulberry@google.com>
2019-09-03 05:59:19 -0700

    Comment change

--- pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
+++ pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
@@ -1,20 +1,15 @@
 List<int> findPrimes(int last) {
   List<int> primes = [];
   for (int x = 2; x < last; x++) {
-    bool isPrime = true;
+    bool isPrime = true; // KEEP?
     for (int i = 0; i < primes.length; i++) {
       int p = primes[i];
-      if (p * p > x) {
-        break;
-      }
+      if (p * p > x) { // KEEP?
+        break; // KEEP?
+      } // KEEP?
       if (x % p == 0) {
-        isPrime = false;
-//        break;
+        isPrime = false; // KEEP?
       }
     }
-//    if (isPrime) {
-//      primes.add(x);
-//    }
   }
-//  return primes;
 }
