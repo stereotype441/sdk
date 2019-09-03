dc1791781b587e318646288023ed3789e7b38664
Paul Berry <paulberry@google.com>
2019-09-03 05:56:32 -0700

    Test passes now

--- pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
+++ pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
@@ -1,20 +1,20 @@
 List<int> findPrimes(int last) {
   List<int> primes = [];
   for (int x = 2; x < last; x++) {
-    bool isPrime = true;
+//    bool isPrime = true;
     for (int i = 0; i < primes.length; i++) {
       int p = primes[i];
-      if (p * p > x) {
-        break;
-      }
+//      if (p * p > x) {
+//        break;
+//      }
       if (x % p == 0) {
-        isPrime = false;
-        break;
+//        isPrime = false;
+//        break;
       }
     }
-    if (isPrime) {
-      primes.add(x);
-    }
+//    if (isPrime) {
+//      primes.add(x);
+//    }
   }
-  return primes;
+//  return primes;
 }
