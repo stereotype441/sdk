b801c96755f8bb9f7c1a1d3e84549e37a1153d89
Paul Berry <paulberry@google.com>
2019-09-03 06:46:50 -0700

    Still fails

--- pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
+++ pkg/front_end/test/flow_analysis/type_promotion/data/foo.dart
@@ -1,6 +1,6 @@
 void findPrimes() {
-  for (int x = 2; x < 10; x++) {
-    bool isPrime = true; // KEEP?
-    isPrime = false; // KEEP?
+  for (int x = 0; x < 10; x++) {
+    bool b = true;
+    b = false;
   }
 }
