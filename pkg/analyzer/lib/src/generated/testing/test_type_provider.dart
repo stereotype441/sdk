06d58b626ce6dcd7bee7df81fc3445ef7c682d30
Paul Berry <paulberry@google.com>
2019-09-05 08:38:59 -0700

    Maybe unnecessary

--- pkg/analyzer/lib/src/generated/testing/test_type_provider.dart
+++ pkg/analyzer/lib/src/generated/testing/test_type_provider.dart
@@ -616,7 +616,7 @@ class TestTypeProvider extends TypeProviderBase {
       asyncSource = _context.sourceFactory.forUri(DartSdk.DART_ASYNC);
       _context.setContents(asyncSource, "");
     } else {
-      asyncSource = null;
+      asyncSource = StringSource('', '/dart_async.dart', uri: Uri.parse(DartSdk.DART_ASYNC));
     }
     CompilationUnitElementImpl asyncUnit = new CompilationUnitElementImpl();
     LibraryElementImpl asyncLibrary = new LibraryElementImpl.forNode(
