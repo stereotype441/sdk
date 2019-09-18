42cdeffb0f06f85d50c649b48743d2e2e1da114d
Paul Berry <paulberry@google.com>
2019-09-18 15:18:33 -0700

    Back out a fix

--- pkg/nnbd_migration/lib/src/edge_builder.dart
+++ pkg/nnbd_migration/lib/src/edge_builder.dart
@@ -1731,10 +1731,7 @@ class EdgeBuilder extends GeneralizingAstVisitor<DecoratedType>
     _currentFunctionType = _variables.decoratedElementType(declaredElement);
     _addParametersToFlowAnalysis(parameters);
     // Push a scope of post-dominated declarations on the stack.
-    _postDominatedLocals.pushScope(elements: [
-      for (var param in declaredElement.parameters)
-        if (!param.isInitializingFormal) param
-    ]);
+    _postDominatedLocals.pushScope(elements: declaredElement.parameters);
     try {
       initializers?.accept(this);
       body.accept(this);
