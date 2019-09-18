66ac5aea64fd3b8c7cee400485333359d52fc7af
Paul Berry <paulberry@google.com>
2019-09-18 15:14:22 -0700

    Back out "fix"

--- pkg/nnbd_migration/test/api_test.dart
+++ pkg/nnbd_migration/test/api_test.dart
@@ -90,6 +90,7 @@ main() {
     await _checkSingleFileChanges(content, expected);
   }
 
+  @FailingTest(issue: 'https://github.com/dart-lang/sdk/issues/38341')
   test_back_propagation_stops_at_implicitly_typed_variables() async {
     var content = '''
 class C {
