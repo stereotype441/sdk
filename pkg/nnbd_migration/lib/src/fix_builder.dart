7937ff208f727c54970a06fcc7455bf7c9ddfcb9
Paul Berry <paulberry@google.com>
2019-09-20 07:21:58 -0700

    WIP

--- pkg/nnbd_migration/lib/src/fix_builder.dart
+++ pkg/nnbd_migration/lib/src/fix_builder.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/precedence.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
@@ -14,6 +15,7 @@ import 'package:analyzer/src/dart/element/type.dart';
 import 'package:analyzer/src/dart/resolver/flow_analysis_visitor.dart';
 import 'package:analyzer/src/generated/resolver.dart';
 import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer_plugin/protocol/protocol_common.dart' show SourceEdit;
 import 'package:front_end/src/fasta/flow_analysis/flow_analysis.dart';
 import 'package:meta/meta.dart';
 import 'package:nnbd_migration/instrumentation.dart';
@@ -28,34 +30,101 @@ import 'package:nnbd_migration/src/nullability_node.dart';
 import 'package:nnbd_migration/src/utilities/annotation_tracker.dart';
 import 'package:nnbd_migration/src/utilities/permissive_mode.dart';
 import 'package:nnbd_migration/src/utilities/scoped_set.dart';
-import 'package:analyzer_plugin/protocol/protocol_common.dart' show SourceEdit;
+
+abstract class FixBuilder extends GeneralizingAstVisitor<_VisitResult> {
+  IfBehavior getIfBehavior(IfStatement node);
+
+  bool needsNullCheck(Expression node);
+
+  List<SourceEdit> run(CompilationUnit unit) {
+    return unit.accept(this).edits;
+  }
+
+  @override
+  _VisitResult visitExpression(Expression node) {
+    var result = super.visitExpression(node)..precedence = node.precedence;
+    if (needsNullCheck(node)) {
+      if (result.precedence < Precedence.postfix) {
+        result.prefix('(');
+        result.suffix(')!');
+        result.precedence = Precedence.postfix;
+      } else {
+        result.suffix('!');
+      }
+    }
+    return result;
+  }
+
+  @override
+  _VisitResult visitIfStatement(IfStatement node) {
+    switch (getIfBehavior(node)) {
+      case IfBehavior.keepAll:
+        return super.visitIfStatement(node);
+      case IfBehavior.keepConditionAndThen:
+        return _sequenceStatements([node.condition, node.thenStatement]);
+      case IfBehavior.keepConditionAndElse:
+        if (node.elseStatement != null) {
+          return _sequenceStatements([node.condition, node.elseStatement]);
+        } else {
+          return _extractStatement(node.condition);
+        }
+        break;
+      case IfBehavior.keepThen:
+        return _extractStatement(node.thenStatement);
+      case IfBehavior.keepElse:
+        if (node.elseStatement != null) {
+          return _extractStatement(node.elseStatement);
+        } else {
+          return _discardStatement();
+        }
+        break;
+    }
+    throw StateError('Unexpected if behavior');
+  }
+
+  @override
+  _VisitResult visitNode(AstNode node) {
+    var result = _VisitResult(node.offset, node.end);
+    for (var entity in node.childEntities) {
+      if (entity is AstNode) {
+        var subResult = entity.accept(this);
+        result.merge(subResult);
+      }
+    }
+    return result;
+  }
+
+  _VisitResult _discardStatement() {
+    throw UnimplementedError('TODO(paulberry)');
+  }
+
+  _VisitResult _extractStatement(AstNode node) {
+    throw UnimplementedError('TODO(paulberry)');
+  }
+
+  _VisitResult _sequenceStatements(List<AstNode> nodes) {
+    throw UnimplementedError('TODO(paulberry)');
+  }
+}
+
+enum IfBehavior {
+  keepAll,
+  keepConditionAndThen,
+  keepConditionAndElse,
+  keepThen,
+  keepElse
+}
 
 class _VisitResult {
   final int offset;
 
   final int end;
 
-  bool needsParens;
-
   List<SourceEdit> edits = [];
 
-  _VisitResult(this.offset, this.end, this.needsParens);
+  Precedence precedence;
 
-  void prefix(String s) {
-    if (edits.first.offset == offset) {
-      edits.first.replacement = s + edits.first.replacement;
-    } else {
-      edits.insert(0, SourceEdit(offset, 0, s));
-    }
-  }
-
-  void suffix(String s) {
-    if (edits.last.end == end) {
-      edits.last.replacement += s;
-    } else {
-      edits.add(SourceEdit(end, 0, s));
-    }
-  }
+  _VisitResult(this.offset, this.end);
 
   void merge(_VisitResult subResult) {
     var subEdits = subResult.edits;
@@ -73,115 +142,20 @@ class _VisitResult {
       edits.addAll(subEdits);
     }
   }
-}
 
-enum IfBehavior {
-  keepAll,
-  keepConditionAndThen,
-  keepConditionAndElse,
-  keepThen,
-  keepElse
-}
-
-abstract class FixBuilder extends GeneralizingAstVisitor<_VisitResult>
-    with
-        PermissiveModeVisitor<_VisitResult>,
-        AnnotationTracker<_VisitResult> {
-  @override
-  final NullabilityMigrationListener listener;
-
-  @override
-  final Source source;
-
-  FixBuilder(this.listener, this.source);
-
-  @override
-  _VisitResult visitExpression(Expression node) {
-    var result = super.visitExpression(node);
-    if (_needsNullCheck(node)) {
-      if (result.needsParens) {
-        result.prefix('(');
-        result.suffix(')!');
-        result.needsParens = false;
-      } else {
-        result.suffix('!');
-      }
-    }
-    return result;
-  }
-
-  @override
-  _VisitResult visitNode(AstNode node) {
-    var result = _VisitResult(node.offset, node.end, true);
-    for (var entity in node.childEntities) {
-      if (entity is AstNode) {
-        var subResult = entity.accept(this);
-        result.merge(subResult);
-      }
-    }
-    return result;
-  }
-
-  IfBehavior getIfBehavior(IfStatement node);
-
-  @override
-  _VisitResult visitIfStatement(IfStatement node) {
-    switch (getIfBehavior(node)) {
-      case IfBehavior.keepAll:
-        return super.visitIfStatement(node);
-      case IfBehavior.keepConditionAndThen:
-        return _sequenceStatements(node.condition, node.thenStatement);
-      case IfBehavior.keepConditionAndElse:
-        if (node.elseStatement != null) {
-          TODO;
-        }
-        // TODO: Handle this case.
-        break;
-      case IfBehavior.keepThen:
-        // TODO: Handle this case.
-        break;
-      case IfBehavior.keepElse:
-        // TODO: Handle this case.
-        break;
-    }
-    var conditionalDiscard = _getConditionalDiscard(node);
-    // keep  pure  hasFalse behavior
-    // both  DC    DC       unchanged
-    // true  true  DC       keep true only
-    // true  false DC       sequence condition and true
-    // false true  true     keep false only
-    // false true  false    eliminate
-    // false false true     sequence condition and false
-    // false false false    keep condition only
-    if (conditionalDiscard.keepTrue) {
-      if (conditionalDiscard.keepFalse) {
-        return super.visitIfStatement(node);
-      } else if (conditionalDiscard.pureCondition) {
-        return _extractStatement(node.thenStatement);
-      } else {
-        return _sequenceStatements(node.condition, node.thenStatement);
-      }
+  void prefix(String s) {
+    if (edits.isNotEmpty && edits.first.offset == offset) {
+      edits.first.replacement = s + edits.first.replacement;
     } else {
-      assert(conditionalDiscard.keepFalse);
-      if (conditionalDiscard.pureCondition) {
-        if (node.elseStatement != null) {
-          return _extractStatement(node.elseStatement);
-        } else {
-          return _discardStatement();
-        }
-      } else {
-        if (node.elseStatement != null) {
-          return _sequenceStatements(node.condition, node.elseStatement);
-        } else {
-          return _extractStatement(node.condition);
-        }
-      }
+      edits.insert(0, SourceEdit(offset, 0, s));
     }
   }
 
-  bool _needsNullCheck(Expression node) {
-    throw UnimplementedError('TODO(paulberry)');
+  void suffix(String s) {
+    if (edits.isNotEmpty && edits.last.end == end) {
+      edits.last.replacement += s;
+    } else {
+      edits.add(SourceEdit(end, 0, s));
+    }
   }
-
-  ConditionalDiscard _getConditionalDiscard(AstNode node) {}
 }
---
+++ pkg/nnbd_migration/test/fix_builder_test.dart
@@ -0,0 +1,99 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/src/dart/element/element.dart';
+import 'package:analyzer/src/dart/element/member.dart';
+import 'package:analyzer/src/dart/element/type.dart';
+import 'package:analyzer/src/generated/resolver.dart';
+import 'package:analyzer/src/generated/testing/test_type_provider.dart';
+import 'package:nnbd_migration/src/decorated_class_hierarchy.dart';
+import 'package:nnbd_migration/src/decorated_type.dart';
+import 'package:nnbd_migration/src/edge_builder.dart';
+import 'package:nnbd_migration/src/edge_origin.dart';
+import 'package:nnbd_migration/src/expression_checks.dart';
+import 'package:nnbd_migration/src/fix_builder.dart';
+import 'package:nnbd_migration/src/nullability_node.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/test_utilities/find_node.dart';
+import 'package:nnbd_migration/instrumentation.dart';
+import 'package:nnbd_migration/nnbd_migration.dart';
+import 'package:nnbd_migration/nullability_state.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+import 'package:analyzer/dart/analysis/features.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/file_system/memory_file_system.dart';
+import 'package:analyzer/src/test_utilities/resource_provider_mixin.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+import 'package:analyzer_plugin/protocol/protocol_common.dart';
+import 'package:nnbd_migration/nnbd_migration.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(FixBuilderTest);
+  });
+}
+
+class _FixBuilder extends FixBuilder {
+  final Map<IfStatement, IfBehavior> _ifBehavior = {};
+
+  final Set<Expression> _nullCheck = {};
+
+  @override
+  IfBehavior getIfBehavior(IfStatement node) => _ifBehavior[node] ?? IfBehavior.keepAll;
+
+  @override
+  bool needsNullCheck(Expression node) => _nullCheck.contains(node);
+}
+
+@reflectiveTest
+class FixBuilderTest {
+  String content;
+
+  FindNode findNode;
+
+  final fixBuilder = _FixBuilder();
+
+  CompilationUnit unit;
+
+  test_null_check_simple() {
+    parse('''
+var x = y;
+''');
+    nullCheck(findNode.simple('y'));
+    checkResult('''
+var x = y!;
+''');
+  }
+
+  void parse(String content) {
+    this.content = content;
+    var parseResult = parseString(content: content);
+    expect(parseResult.errors, isEmpty);
+    findNode = FindNode(content, parseResult.unit);
+    unit = parseResult.unit;
+  }
+
+  void nullCheck(Expression expression) {
+    fixBuilder._nullCheck.add(expression);
+  }
+
+  void checkResult(String expected) {
+    var edits = fixBuilder.run(unit);
+    expect(SourceEdit.applySequence(content, edits.reversed),
+        expected);
+  }
+}
