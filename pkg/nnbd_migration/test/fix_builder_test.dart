32381c79b5dd4355b76b5493e29140c32368f71a
Paul Berry <paulberry@google.com>
2019-09-27 04:07:41 -0700

    String literal

---
+++ pkg/nnbd_migration/test/fix_builder_test.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/src/dart/element/type.dart';
+import 'package:nnbd_migration/src/fix_builder.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import 'migration_visitor_test_base.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(FixBuilderTest);
+  });
+}
+
+@reflectiveTest
+class FixBuilderTest extends EdgeBuilderTestBase {
+  test_stringLiteral() async {
+    await analyze('''
+f() => 'foo';
+''');
+    visit(findNode.stringLiteral('foo'), 'String');
+  }
+
+  DartType visit(AstNode node, String expectedType) {
+    var type = node.accept(FixBuilder());
+    expect((type as TypeImpl).toString(withNullability: true), expectedType);
+    return type;
+  }
+}
