e245e0589cd10fbed745f7617d2a35c7d1afe24a
Paul Berry <paulberry@google.com>
2019-09-27 06:11:16 -0700

    WIP on flow analysis

--- pkg/nnbd_migration/test/fix_builder_test.dart
+++ pkg/nnbd_migration/test/fix_builder_test.dart
@@ -40,6 +40,16 @@ f() {
     visitSubexpression(findNode.binary('&&'), 'bool');
   }
 
+  test_binaryExpression_ampersand_ampersand_flow() async {
+    await analyze('''
+f() {
+  bool x = null;
+  return x != null && x;
+}
+''');
+    visitSubexpression(findNode.binary('&&'), 'bool');
+  }
+
   test_binaryExpression_ampersand_ampersand_nullChecked() async {
     await analyze('''
 f() {
