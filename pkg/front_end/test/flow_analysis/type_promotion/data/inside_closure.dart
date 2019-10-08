cc3bd70a0157d11894b7eca049da9ec13c988fb2
Paul Berry <paulberry@google.com>
2019-10-08 16:28:02 -0700

    Fix

--- pkg/front_end/test/flow_analysis/type_promotion/data/inside_closure.dart
+++ pkg/front_end/test/flow_analysis/type_promotion/data/inside_closure.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-void parameter([Function(dynamic)? f]) {
+void parameter(Function([dynamic]) f) {
   void inner(Object x) {
     if (x is String) {
       f();
