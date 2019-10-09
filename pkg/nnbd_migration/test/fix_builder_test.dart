d4740a08e31fcd93fe85523de1a7b73f12a09f12
Paul Berry <paulberry@google.com>
2019-10-09 13:30:15 -0700

    De-hack

--- pkg/nnbd_migration/test/fix_builder_test.dart
+++ pkg/nnbd_migration/test/fix_builder_test.dart
@@ -347,7 +347,7 @@ _f(bool x, bool y) => x && y;
     visitSubexpression(findNode.binary('&&'), 'bool');
   }
 
-  solo_test_binaryExpression_ampersand_ampersand_flow() async {
+  test_binaryExpression_ampersand_ampersand_flow() async {
     await analyze('''
 _f(bool/*?*/ x) => x != null && x;
 ''');
