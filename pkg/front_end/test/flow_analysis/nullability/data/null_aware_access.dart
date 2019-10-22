962287b222adffba0785b7af87ae6c1089d8e9e6
Paul Berry <paulberry@google.com>
2019-10-22 13:43:39 +0200

    Postpone null shorting with method calls

--- pkg/front_end/test/flow_analysis/nullability/data/null_aware_access.dart
+++ pkg/front_end/test/flow_analysis/nullability/data/null_aware_access.dart
@@ -3,12 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 class C {
-  int methodReturningInt([value]) => 0;
   void set setter(value) {}
   C operator[](index) => this;
   void operator[]=(index, value) {}
-  C methodReturningC() => this;
-  C? methodReturningNullableC() => this;
   C get getterReturningC => this;
   C? get getterReturningNullableC => this;
 }
@@ -17,11 +14,6 @@ class D {
   void set setter(value) {}
   D? operator[](index) => this;
   void operator[]=(index, value) {}
-  int methodReturningInt([value]) => 0;
-}
-
-void methodCall(C? c) {
-  c?.methodReturningInt(/*nonNullable*/ c);
 }
 
 void setterCall(C? c) {
@@ -36,18 +28,7 @@ void indexSetterCall(C? c) {
   c?.[/*nonNullable*/ c] = /*nonNullable*/ c;
 }
 
-void methodCall_nullShorting(C? c, D? d) {
-  c?.methodReturningC().methodReturningInt(/*nonNullable*/ c);
-  c?.methodReturningNullableC()?.methodReturningInt(/*nonNullable*/ c);
-  c?.getterReturningC.methodReturningInt(/*nonNullable*/ c);
-  c?.getterReturningNullableC?.methodReturningInt(/*nonNullable*/ c);
-  c?.[0].methodReturningInt(/*nonNullable*/ c);
-  d?.[0]?.methodReturningInt(/*nonNullable*/ d);
-}
-
 void setterCall_nullShorting(C? c, D? d) {
-  c?.methodReturningC().setter = /*nonNullable*/ c;
-  c?.methodReturningNullableC()?.setter = /*nonNullable*/ c;
   c?.getterReturningC.setter = /*nonNullable*/ c;
   c?.getterReturningNullableC?.setter = /*nonNullable*/ c;
   c?.[0].setter = /*nonNullable*/ c;
@@ -55,8 +36,6 @@ void setterCall_nullShorting(C? c, D? d) {
 }
 
 void indexGetterCall_nullShorting(C? c, D? d) {
-  c?.methodReturningC()[/*nonNullable*/ c];
-  c?.methodReturningNullableC()?.[/*nonNullable*/ c];
   c?.getterReturningC[/*nonNullable*/ c];
   c?.getterReturningNullableC?.[/*nonNullable*/ c];
   c?.[0][/*nonNullable*/ c];
@@ -64,8 +43,6 @@ void indexGetterCall_nullShorting(C? c, D? d) {
 }
 
 void indexSetterCall_nullShorting(C? c, D? d) {
-  c?.methodReturningC()[/*nonNullable*/ c] = /*nonNullable*/ c;
-  c?.methodReturningNullableC()?.[/*nonNullable*/ c] = /*nonNullable*/ c;
   c?.getterReturningC[/*nonNullable*/ c] = /*nonNullable*/ c;
   c?.getterReturningNullableC?.[/*nonNullable*/ c] = /*nonNullable*/ c;
   c?.[0][/*nonNullable*/ c] = /*nonNullable*/ c;
