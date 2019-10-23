4ce2a5647a062b107f1b3554c1b2134334377c80
Paul Berry <paulberry@google.com>
2019-10-23 08:12:01 +0200

    New tests

---
+++ pkg/front_end/test/flow_analysis/type_promotion/data/type_parameter.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class C<T extends num> {
+  void promotes(T t) {
+    if (t is int) {
+      /*T & int*/ t;
+    }
+  }
+  void doesNotPromote(T t) {
+    if (t is String) {
+      t;
+    }
+  }
+}
