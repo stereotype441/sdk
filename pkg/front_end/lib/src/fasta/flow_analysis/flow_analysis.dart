a3fdf752d43ef25b62d9490deb5f5376215cabb2
Paul Berry <paulberry@google.com>
2019-10-25 14:15:42 +0200

    Fix

--- pkg/front_end/lib/src/fasta/flow_analysis/flow_analysis.dart
+++ pkg/front_end/lib/src/fasta/flow_analysis/flow_analysis.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/dart/ast/ast.dart';
 import 'package:meta/meta.dart';
 
 /// [AssignedVariables] is a helper class capable of computing the set of
@@ -181,7 +180,7 @@ class AssignedVariablesForTesting<Node, Variable>
     super.endNode(node, isClosure: isClosure);
   }
 
-  bool isTracked(AstNode node) => _writtenInNode.containsKey(node);
+  bool isTracked(Node node) => _writtenInNode.containsKey(node);
 
   Set<Variable> writtenInNode(Node node) => _getWrittenInNode(node);
 }
