1780da47c20ddd7e61e6eac9238da3bbc8a1d177
Paul Berry <paulberry@google.com>
2019-10-25 16:35:10 -0700

    dartfmt

--- pkg/analyzer/lib/src/task/strong/checker.dart
+++ pkg/analyzer/lib/src/task/strong/checker.dart
@@ -963,8 +963,7 @@ class CodeChecker extends RecursiveAstVisitor {
   }
 
   void _checkUnary(Expression operand, Token op, MethodElement element) {
-    bool isIncrementAssign =
-        op.type.isIncrementOperator;
+    bool isIncrementAssign = op.type.isIncrementOperator;
     if (op.isUserDefinableOperator || isIncrementAssign) {
       if (element == null) {
         _recordDynamicInvoke(operand.parent, operand);
