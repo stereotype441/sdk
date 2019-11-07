02c89d5799b2fccaed97160d76586caf90778411
Paul Berry <paulberry@google.com>
2019-11-07 16:26:32 +0100

    More tests

--- pkg/_fe_analyzer_shared/test/flow_analysis/nullability/data/b39209.dart
+++ pkg/_fe_analyzer_shared/test/flow_analysis/nullability/data/b39209.dart
@@ -1,11 +1,27 @@
-propertyAccess(int? x) {
-  x?..isEven..isEven;
+propertyGet(C? c) {
+  c?..property.f(/*nonNullable*/ c)..method(/*nonNullable*/ c);
+}
+
+propertySet(C? c) {
+  c?..property = /*nonNullable*/ c..method(/*nonNullable*/ c);
 }
 
 methodCall(C? c) {
-  c?..f(/*nonNullable*/ c)..f(/*nonNullable*/ c);
+  c?..method(/*nonNullable*/ c)..method(/*nonNullable*/ c);
+}
+
+indexGet(C? c) {
+  c?..[/*nonNullable*/ c].f(/*nonNullable*/ c)..method(/*nonNullable*/ c);
+}
+
+indexSet(C? c) {
+  c?..[/*nonNullable*/ c] = /*nonNullable*/ c..method(/*nonNullable*/ c);
 }
 
 abstract class C {
-  void f(argument);
+  dynamic get property;
+  void set property(dynamic value);
+  void method(dynamic argument);
+  dynamic operator[](dynamic index);
+  void operator[]=(dynamic index, dynamic value);
 }
