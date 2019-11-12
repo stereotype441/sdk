f365c70755c081c1d3e1d301c9db1c8722ebc203
Paul Berry <paulberry@google.com>
2019-11-12 19:34:42 +0100

    WIP

--- pkg/nnbd_migration/lib/src/edge_builder.dart
+++ pkg/nnbd_migration/lib/src/edge_builder.dart
@@ -1622,7 +1622,9 @@ class EdgeBuilder extends GeneralizingAstVisitor<DecoratedType>
             '(${expression.toSource()}) offset=${expression.offset}');
       }
       EdgeOrigin edgeOrigin;
-      if (!sourceType.type.isDynamic) {
+      if (sourceType.type.isDynamic) {
+        edgeOrigin = DynamicAssignmentOrigin(source, expression);
+      } else {
         if (fromDefaultValue) {
           edgeOrigin = DefaultValueOrigin(source, expression);
         } else {
