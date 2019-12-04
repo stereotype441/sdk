1589c8458f8d5a9bfab621fb2e81c982d49469d5
Paul Berry <paulberry@google.com>
2019-12-03 16:36:10 -0800

    WIP

--- pkg/nnbd_migration/test/fix_builder_test.dart
+++ pkg/nnbd_migration/test/fix_builder_test.dart
@@ -2109,7 +2109,7 @@ void _f(bool/*?*/ x, bool/*?*/ y) {
       Map<AstNode, Set<Problem>> problems = const <AstNode, Set<Problem>>{}}) {
     _FixBuilder fixBuilder = _FixBuilder(node, testSource,
         decoratedClassHierarchy, typeProvider, typeSystem, variables);
-    node.thisOrAncestorOfType<CompilationUnit>().accept(fixBuilder);
+    fixBuilder.visitAll(node.thisOrAncestorOfType<CompilationUnit>());
     var type = fixBuilder.expressionType[node];
     expect((type as TypeImpl).toString(withNullability: true), expectedType);
     expect(fixBuilder.changes, changes);
