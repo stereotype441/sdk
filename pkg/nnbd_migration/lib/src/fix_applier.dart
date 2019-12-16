20405bf6525dea95ded29379a3eb7623de97445f
Paul Berry <paulberry@google.com>
2019-12-16 15:42:25 -0800

    WIP

--- pkg/nnbd_migration/lib/src/fix_applier.dart
+++ pkg/nnbd_migration/lib/src/fix_applier.dart
@@ -7,67 +7,130 @@ import 'package:analyzer/dart/ast/precedence.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:nnbd_migration/src/fix_builder.dart';
 
-/// TODO(paulberry): this seems like a horrible hack.
-extension _CorrectedPrecedence on Expression {
-  Precedence get correctedPrecedence {
-    var node = this;
-    if (node is ThrowExpression) {
-      var subExpression = node.expression;
-      if (subExpression is CascadeExpression) {
-        return Precedence.cascade;
-      }
-    }
-    return precedence;
-  }
+class AddBang extends PreviewInfo {
+  const AddBang();
 }
 
-abstract class _Change {
+class AddCloseParen extends PreviewInfo {
+  const AddCloseParen();
+}
+
+class AddOpenParen extends PreviewInfo {
+  const AddOpenParen();
+}
+
+abstract class Change {
+  const Change();
+
   _Plan apply(AstNode node, FixPlanner planner);
 }
 
-abstract class _NestableChange extends _Change {
-  final _Change _inner;
+/// TODO(paulberry): rename file to fix_planner.dart?
+class FixPlanner extends GeneralizingAstVisitor<_Plan> {
+  final Map<AstNode, Change> _changes;
 
-  _NestableChange(this._inner);
+  FixPlanner._(this._changes);
+
+  _Plan visitBinaryExpression(BinaryExpression node) {
+    // TODO(paulberry): test
+    var plan = _Plan.empty(node);
+    // TODO(paulberry): fix context
+    _exploratory3(plan, node.leftOperand, context: node.precedence);
+    _exploratory3(plan, node.rightOperand,
+        context: node.precedence, atEnd: true);
+    return plan;
+  }
+
+  _Plan visitExpression(Expression node) {
+    throw UnimplementedError('TODO(paulberry): ${node.runtimeType}');
+  }
+
+  _Plan visitFunctionExpression(FunctionExpression node) {
+    var plan = _Plan.empty(node);
+    _exploratory3(plan, node.typeParameters);
+    _exploratory3(plan, node.parameters);
+    _exploratory3(plan, node.body);
+    return plan;
+  }
+
+  _Plan visitNode(AstNode node) {
+    var plan = _Plan.empty(node);
+    for (var entity in node.childEntities) {
+      if (entity is AstNode) {
+        plan.subsume(_exploratory2(entity));
+      }
+    }
+    return plan;
+  }
+
+  _Plan visitPostfixExpression(PostfixExpression node) {
+    // TODO(paulberry): test
+    var plan = _Plan.empty(node);
+    _exploratory3(plan, node.operand, context: Precedence.postfix);
+    return plan;
+  }
+
+  _Plan visitSimpleIdentifier(Expression node) {
+    return _Plan.empty(node);
+  }
+
+  /// This version passes around a plan
+  /// TODO(paulberry): rename
+  _Plan _exploratory2(AstNode node) {
+    var change = _changes[node] ?? NoChange();
+    return change.apply(node, this);
+  }
+
+  /// TODO(paulberry): combine with _exploratory2?
+  void _exploratory3(_Plan plan, AstNode node,
+      {Precedence context = Precedence.none, bool atEnd: false}) {
+    if (node == null) return;
+    // TODO(paulberry): test adding of parens
+    var subPlan = _exploratory2(node).addParensIfLowerPrecedenceThan(context);
+    // TODO(paulberry): test adding of parens for cascade reasons
+    if (atEnd && subPlan.endsInCascade) {
+      plan.endsInCascade = true;
+    }
+    plan.subsume(subPlan);
+  }
+
+  static Map<int, List<PreviewInfo>> run(
+      CompilationUnit unit, Map<AstNode, Change> changes) {
+    var fixPlanner = FixPlanner._(changes);
+    var plan = fixPlanner._exploratory2(unit);
+    return plan._previewInfo;
+  }
 }
 
-class _NoChange extends _Change {
+class NoChange extends Change {
+  const NoChange();
+
   @override
   _Plan apply(AstNode node, FixPlanner planner) {
     return node.accept(planner);
   }
 }
 
-class _NullCheck extends _NestableChange {
-  _NullCheck(_Change inner) : super(inner);
+class NullCheck extends _NestableChange {
+  const NullCheck([Change inner = const NoChange()]) : super(inner);
 
   @override
   _Plan apply(AstNode node, FixPlanner planner) {
-    return _Plan.suffix(_inner.apply(node, planner).addParensIfLowerPrecedenceThan(Precedence.postfix), '!', Precedence.postfix, false);
+    return _Plan.suffix(
+        _inner
+            .apply(node, planner)
+            .addParensIfLowerPrecedenceThan(Precedence.postfix),
+        const AddBang(),
+        Precedence.postfix,
+        false);
   }
 }
 
-class _MakeNullable extends _NestableChange {
-  _MakeNullable(_Change inner) : super(inner);
-
-  @override
-  _Plan apply(AstNode node, FixPlanner planner) {
-    return _Plan.suffix(_inner.apply(node, planner), '?', Precedence.primary, false);
-  }
+abstract class PreviewInfo {
+  const PreviewInfo();
 }
 
-class _IntroduceAs extends _NestableChange {
-  _IntroduceAs(_Change inner, this.type) : super(inner);
-
-  final String type;
-
-  @override
-  _Plan apply(AstNode node, FixPlanner planner) {
-    return _Plan.suffix(_inner.apply(node, planner).addParensIfLowerPrecedenceThan(Precedence.bitwiseOr), ' as $type', Precedence.relational, false);
-  }
-}
-
-class _ExtractSubexpression extends _Change {
+class _ExtractSubexpression extends Change {
   AstNode _inner;
 
   _ExtractSubexpression(this._inner);
@@ -78,68 +141,94 @@ class _ExtractSubexpression extends _Change {
   }
 }
 
+class _IntroduceAs extends _NestableChange {
+  final String type;
+
+  _IntroduceAs(Change inner, this.type) : super(inner);
+
+  @override
+  _Plan apply(AstNode node, FixPlanner planner) {
+    return _Plan.suffix(
+        _inner
+            .apply(node, planner)
+            .addParensIfLowerPrecedenceThan(Precedence.bitwiseOr),
+        throw 'TODO(paulberry): as $type',
+        Precedence.relational,
+        false);
+  }
+}
+
+class _MakeNullable extends _NestableChange {
+  _MakeNullable(Change inner) : super(inner);
+
+  @override
+  _Plan apply(AstNode node, FixPlanner planner) {
+    return _Plan.suffix(_inner.apply(node, planner), throw 'TODO(paulberry): ?',
+        Precedence.primary, false);
+  }
+}
+
+abstract class _NestableChange extends Change {
+  final Change _inner;
+
+  const _NestableChange(this._inner);
+}
+
 class _Plan {
-  factory _Plan.suffix(_Plan innerPlan, String affix, Precedence precedence, bool endsInCascade) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
+  bool endsInCascade = false;
 
-  bool get endsInCascade => throw UnimplementedError('TODO(paulberry)');
+  Map<int, List<PreviewInfo>> _previewInfo;
 
-  _Plan addParensIfLowerPrecedenceThan(Precedence p) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
+  final Precedence _precedence;
 
-  factory _Plan.empty(AstNode node, bool endsInCascade) {
-    throw UnimplementedError('TODO(paulberry)');
+  /// TODO(paulberry): should I replace offset and end with a node?
+  final int _offset;
+
+  final int _end;
+
+  _Plan.empty(AstNode node)
+      : _precedence = _computePrecedenceFor(node),
+        _offset = node.offset,
+        _end = node.end;
+
+  _Plan.suffix(
+      _Plan innerPlan, PreviewInfo affix, this._precedence, this.endsInCascade)
+      : _previewInfo = innerPlan._previewInfo,
+        _offset = innerPlan._offset,
+        _end = innerPlan._end {
+    ((_previewInfo ??= {})[innerPlan._end] ??= []).add(affix);
   }
 
   _Plan addParensIfEndsInCascade() {
     throw UnimplementedError('TODO(paulberry)');
   }
 
+  _Plan addParensIfLowerPrecedenceThan(Precedence other) {
+    if (_precedence < other) {
+      ((_previewInfo ??= {})[_offset] ??= []).insert(0, const AddOpenParen());
+      ((_previewInfo ??= {})[_end] ??= []).add(const AddCloseParen());
+    }
+    return this;
+  }
+
   void execute() {
     throw UnimplementedError('TODO(paulberry)');
   }
-}
 
-/// TODO(paulberry): rename file to fix_planner.dart?
-class FixPlanner extends GeneralizingAstVisitor<_Plan> {
-  final Map<AstNode, _Change> _changes;
-
-  /// This version passes around a plan
-  _Plan _exploratory2(AstNode node) {
-    var change = _changes[node] ?? _NoChange();
-    return change.apply(node, this);
-  }
-
-  Precedence _getContextPrecedence(AstNode node){
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  bool _getContextDisallowsCascade(AstNode node) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  bool _getContextIsAtEnd(AstNode node) {
-    throw UnimplementedError('TODO(paulberry)');
-  }
-
-  _Plan visitNode(AstNode node) {
-    bool endsInCascade = false;
-    for (var entity in node.childEntities) {
-      if (entity is AstNode) {
-        var subPlan = _exploratory2(entity).addParensIfLowerPrecedenceThan(_getContextPrecedence(entity));
-        if (_getContextDisallowsCascade(entity)) {
-          subPlan = subPlan.addParensIfEndsInCascade();
-        }
-        if (_getContextIsAtEnd(entity) && subPlan.endsInCascade) {
-          endsInCascade = true;
-        }
-      }
+  void subsume(_Plan subPlan) {
+    if (subPlan == null) return;
+    if (_previewInfo == null) {
+      _previewInfo = subPlan._previewInfo;
+    } else {
+      _previewInfo.addAll(subPlan._previewInfo);
     }
-    var plan = _Plan.empty(node, endsInCascade);
-    return plan;
   }
 
-  FixPlanner(this._changes);
+  static Precedence _computePrecedenceFor(AstNode node) {
+    // TODO(paulberry): special handling of cascades
+    if (node is Expression) return node.precedence;
+    // For non-expressions we can just use Precedence.primary since no adding of
+    // parens is necessary.
+    return Precedence.primary;
+  }
 }
---
+++ pkg/nnbd_migration/test/fix_planner_test.dart
@@ -0,0 +1,72 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/src/dart/element/type.dart';
+import 'package:analyzer/src/dart/element/type_provider.dart';
+import 'package:analyzer/src/generated/resolver.dart';
+import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/generated/type_system.dart';
+import 'package:analyzer/src/task/strong/checker.dart';
+import 'package:nnbd_migration/src/decorated_class_hierarchy.dart';
+import 'package:nnbd_migration/src/fix_applier.dart';
+import 'package:nnbd_migration/src/variables.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import 'abstract_single_unit.dart';
+import 'migration_visitor_test_base.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(FixPlannerTest);
+  });
+}
+
+@reflectiveTest
+class FixPlannerTest extends AbstractSingleUnitTest {
+  void test_adjacentFixes() async {
+    await resolveTestUnit('''
+f(a, b) => a + b;
+''');
+    var aRef = findNode.simple('a +');
+    var bRef = findNode.simple('b;');
+    var previewInfo = FixPlanner.run(testUnit, {
+      aRef: const NullCheck(),
+      bRef: const NullCheck(),
+      findNode.binary('a + b'): const NullCheck()
+    });
+    expect(previewInfo, {
+      aRef.offset: [const AddOpenParen()],
+      aRef.end: [const AddBang()],
+      bRef.end: [const AddBang(), const AddCloseParen(), const AddBang()]
+    });
+  }
+
+  void test_nullCheck_no_parens() async {
+    await resolveTestUnit('''
+f(a) => a++;
+''');
+    var postfix = findNode.postfix('a++');
+    var previewInfo = FixPlanner.run(testUnit, {postfix: const NullCheck()});
+    expect(previewInfo, {
+      postfix.end: [const AddBang()]
+    });
+  }
+
+  void test_nullCheck_parens() async {
+    await resolveTestUnit('''
+f(a) => ++a;
+''');
+    var prefix = findNode.prefix('-a');
+    var previewInfo = FixPlanner.run(testUnit, {prefix: const NullCheck()});
+    expect(previewInfo, {
+      prefix.offset: [const AddOpenParen()],
+      prefix.end: [const AddCloseParen(), const AddBang()]
+    });
+  }
+}
