5754bbdc3fe0b17d240715b8690a8271f5421244
Paul Berry <paulberry@google.com>
2019-12-19 09:12:13 -0800

    Clean up tests

--- pkg/nnbd_migration/test/fix_planner_test.dart
+++ pkg/nnbd_migration/test/fix_planner_test.dart
@@ -27,9 +27,7 @@ g(a, b) => a | b as int;
   }
 
   void test_precedence_assignment() async {
-    await _checkPrecedence('''
-f(a, b, c) => a = b = c;
-''');
+    await _checkPrecedence('f(a, b, c) => a = b = c;');
   }
 
   void test_precedence_binary_equality() async {
@@ -85,9 +83,7 @@ g(a) => (-a)?.b;
   }
 
   void test_precedence_throw() async {
-    await _checkPrecedence('''
-f(a, b) => throw a = b;
-''');
+    await _checkPrecedence('f(a, b) => throw a = b;');
   }
 
   void _checkPrecedence(String content) async {
@@ -95,8 +91,8 @@ f(a, b) => throw a = b;
     // remove parens to code that is not being otherwise modified, so we can
     // verify correct precedence by simply running the fix planner over the
     // code with no changes requested.
-    await resolveTestUnit(content);
-    var previewInfo = _run({});
+    await analyze(content);
+    var previewInfo = run({});
     expect(previewInfo, isNull);
   }
 }
@@ -104,271 +100,173 @@ f(a, b) => throw a = b;
 @reflectiveTest
 class FixPlannerTest extends FixPlannerTestBase {
   void test_adjacentFixes() async {
-    await resolveTestUnit('''
-f(a, b) => a + b;
-''');
+    await analyze('f(a, b) => a + b;');
     var aRef = findNode.simple('a +');
     var bRef = findNode.simple('b;');
-    var previewInfo = _run({
+    var previewInfo = run({
       aRef: const NullCheck(),
       bRef: const NullCheck(),
       findNode.binary('a + b'): const NullCheck()
     });
-    expect(previewInfo, {
-      aRef.offset: [const AddOpenParen()],
-      aRef.end: [const AddBang()],
-      bRef.end: [const AddBang(), const AddCloseParen(), const AddBang()]
-    });
+    expect(previewInfo.applyTo(code), 'f(a, b) => (a! + b!)!;');
   }
 
   void test_introduceAs_distant_parens_no_longer_needed() async {
     // Note: in principle it would be nice to delete the outer parens, but it's
     // difficult to see that they used to be necessary and aren't anymore, so we
     // leave them.
-    await resolveTestUnit('''
-f(a, c) => a..b = (throw c..d);
-''');
+    await analyze('f(a, c) => a..b = (throw c..d);');
     var cd = findNode.cascade('c..d');
-    var previewInfo = _run({cd: const IntroduceAs('int')});
-    expect(previewInfo, {
-      cd.offset: [const AddOpenParen()],
-      cd.end: [const AddCloseParen(), AddAs('int')]
-    });
+    var previewInfo = run({cd: const IntroduceAs('int')});
+    expect(
+        previewInfo.applyTo(code), 'f(a, c) => a..b = (throw (c..d) as int);');
   }
 
   void test_introduceAs_no_parens() async {
-    await resolveTestUnit('''
-f(a, b) => a | b;
-''');
+    await analyze('f(a, b) => a | b;');
     var expr = findNode.binary('a | b');
-    var previewInfo = _run({expr: const IntroduceAs('int')});
-    expect(previewInfo, {
-      expr.end: [AddAs('int')]
-    });
+    var previewInfo = run({expr: const IntroduceAs('int')});
+    expect(previewInfo.applyTo(code), 'f(a, b) => a | b as int;');
   }
 
   void test_introduceAs_parens() async {
-    await resolveTestUnit('''
-f(a, b) => a < b;
-''');
+    await analyze('f(a, b) => a < b;');
     var expr = findNode.binary('a < b');
-    var previewInfo = _run({expr: const IntroduceAs('bool')});
-    expect(previewInfo, {
-      expr.offset: [const AddOpenParen()],
-      expr.end: [const AddCloseParen(), AddAs('bool')]
-    });
+    var previewInfo = run({expr: const IntroduceAs('bool')});
+    expect(previewInfo.applyTo(code), 'f(a, b) => (a < b) as bool;');
   }
 
   void test_keep_redundant_parens() async {
-    await resolveTestUnit('''
-f(a, b, c) => a + (b * c);
-''');
-    var previewInfo = _run({}, allowRedundantParens: true);
+    await analyze('f(a, b, c) => a + (b * c);');
+    var previewInfo = run({}, allowRedundantParens: true);
     expect(previewInfo, isNull);
   }
 
   void test_makeNullable() async {
-    await resolveTestUnit('''
-f(int x) {}
-''');
+    await analyze('f(int x) {}');
     var typeName = findNode.typeName('int');
-    var previewInfo = _run({typeName: const MakeNullable()});
-    expect(previewInfo, {
-      typeName.end: [const AddQuestion()]
-    });
+    var previewInfo = run({typeName: const MakeNullable()});
+    expect(previewInfo.applyTo(code), 'f(int? x) {}');
   }
 
   void test_nullCheck_no_parens() async {
-    await resolveTestUnit('''
-f(a) => a++;
-''');
+    await analyze('f(a) => a++;');
     var expr = findNode.postfix('a++');
-    var previewInfo = _run({expr: const NullCheck()});
-    expect(previewInfo, {
-      expr.end: [const AddBang()]
-    });
+    var previewInfo = run({expr: const NullCheck()});
+    expect(previewInfo.applyTo(code), 'f(a) => a++!;');
   }
 
   void test_nullCheck_parens() async {
-    await resolveTestUnit('''
-f(a) => -a;
-''');
+    await analyze('f(a) => -a;');
     var expr = findNode.prefix('-a');
-    var previewInfo = _run({expr: const NullCheck()});
-    expect(previewInfo, {
-      expr.offset: [const AddOpenParen()],
-      expr.end: [const AddCloseParen(), const AddBang()]
-    });
+    var previewInfo = run({expr: const NullCheck()});
+    expect(previewInfo.applyTo(code), 'f(a) => (-a)!;');
   }
 
   void test_removeAs_in_cascade_target_no_parens_needed_cascade() async {
-    await resolveTestUnit('''
-f(a) => ((a..b) as dynamic)..c;
-''');
+    await analyze('f(a) => ((a..b) as dynamic)..c;');
     var cascade = findNode.cascade('a..b');
     var cast = cascade.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      cast.parent.offset: [const RemoveText(1)],
-      cascade.parent.offset: [const RemoveText(1)],
-      cascade.end: [const RemoveText(1)],
-      cascade.parent.end: [RemoveText(cast.end - cascade.parent.end)],
-      cast.end: [const RemoveText(1)]
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a) => a..b..c;');
   }
 
   void test_removeAs_in_cascade_target_no_parens_needed_conditional() async {
     // TODO(paulberry): would it be better to keep the parens in this case for
     // clarity, even though they're not needed?
-    await resolveTestUnit('''
-f(a, b, c) => ((a ? b : c) as dynamic)..d;
-''');
+    await analyze('f(a, b, c) => ((a ? b : c) as dynamic)..d;');
     var conditional = findNode.conditionalExpression('a ? b : c');
     var cast = conditional.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      cast.parent.offset: [const RemoveText(1)],
-      conditional.parent.offset: [const RemoveText(1)],
-      conditional.end: [const RemoveText(1)],
-      conditional.parent.end: [RemoveText(cast.end - conditional.parent.end)],
-      cast.end: [const RemoveText(1)]
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => a ? b : c..d;');
   }
 
   void test_removeAs_in_cascade_target_parens_needed_assignment() async {
-    await resolveTestUnit('''
-f(a, b) => ((a = b) as dynamic)..c;
-''');
+    await analyze('f(a, b) => ((a = b) as dynamic)..c;');
     var assignment = findNode.assignment('a = b');
     var cast = assignment.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      assignment.parent.offset: [const RemoveText(1)],
-      assignment.end: [const RemoveText(1)],
-      assignment.parent.end: [RemoveText(cast.end - assignment.parent.end)]
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b) => (a = b)..c;');
   }
 
   void test_removeAs_in_cascade_target_parens_needed_throw() async {
-    await resolveTestUnit('''
-f(a) => ((throw a) as dynamic)..b;
-''');
+    await analyze('f(a) => ((throw a) as dynamic)..b;');
     var throw_ = findNode.throw_('throw a');
     var cast = throw_.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      throw_.parent.offset: [const RemoveText(1)],
-      throw_.end: [const RemoveText(1)],
-      throw_.parent.end: [RemoveText(cast.end - throw_.parent.end)]
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a) => (throw a)..b;');
   }
 
   void test_removeAs_lower_precedence_do_not_remove_inner_parens() async {
-    await resolveTestUnit('''
-f(a, b, c) => (a == b) as Null == c;
-''');
+    await analyze('f(a, b, c) => (a == b) as Null == c;');
     var expr = findNode.binary('a == b');
-    var previewInfo = _run({expr.parent.parent: const RemoveAs()});
-    expect(previewInfo, {
-      expr.parent.end: [RemoveText(expr.parent.parent.end - expr.parent.end)]
-    });
+    var previewInfo = run({expr.parent.parent: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => (a == b) == c;');
   }
 
   void test_removeAs_lower_precedence_remove_inner_parens() async {
-    await resolveTestUnit('''
-f(a, b) => (a == b) as Null;
-''');
+    await analyze('f(a, b) => (a == b) as Null;');
     var expr = findNode.binary('a == b');
-    var previewInfo = _run({expr.parent.parent: const RemoveAs()});
-    expect(previewInfo, {
-      expr.parent.offset: [RemoveText(1)],
-      expr.end: [RemoveText(1)],
-      expr.parent.end: [RemoveText(expr.parent.parent.end - expr.parent.end)]
-    });
+    var previewInfo = run({expr.parent.parent: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b) => a == b;');
   }
 
   void test_removeAs_parens_needed_due_to_cascade() async {
-    await resolveTestUnit('''
-f(a, c) => a..b = throw (c..d) as int;
-''');
+    await analyze('f(a, c) => a..b = throw (c..d) as int;');
     var cd = findNode.cascade('c..d');
     var cast = cd.parent.parent;
-    var throw_ = cast.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      throw_.offset: [const AddOpenParen()],
-      cd.parent.offset: [const RemoveText(1)],
-      cd.end: [const RemoveText(1)],
-      cd.parent.end: [RemoveText(cast.end - cd.parent.end)],
-      throw_.end: [const AddCloseParen()]
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, c) => a..b = (throw c..d);');
   }
 
   void test_removeAs_parens_needed_due_to_cascade_in_conditional_else() async {
-    await resolveTestUnit('''
-f(a, b, c) => a ? b : (c..d) as int;
-''');
+    await analyze('f(a, b, c) => a ? b : (c..d) as int;');
     var cd = findNode.cascade('c..d');
     var cast = cd.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      cd.parent.end: [RemoveText(cast.end - cd.parent.end)],
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => a ? b : (c..d);');
   }
 
   void test_removeAs_parens_needed_due_to_cascade_in_conditional_then() async {
-    await resolveTestUnit('''
-f(a, b, d) => a ? (b..c) as int : d;
-''');
+    await analyze('f(a, b, d) => a ? (b..c) as int : d;');
     var bc = findNode.cascade('b..c');
     var cast = bc.parent.parent;
-    var previewInfo = _run({cast: const RemoveAs()});
-    expect(previewInfo, {
-      bc.parent.end: [RemoveText(cast.end - bc.parent.end)],
-    });
+    var previewInfo = run({cast: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, d) => a ? (b..c) : d;');
   }
 
   void test_removeAs_raise_precedence_do_not_remove_parens() async {
-    await resolveTestUnit('''
-f(a, b, c) => a | (b | c as int);
-''');
+    await analyze('f(a, b, c) => a | (b | c as int);');
     var expr = findNode.binary('b | c');
-    var previewInfo = _run({expr.parent: const RemoveAs()});
-    expect(previewInfo, {
-      expr.end: [RemoveText(expr.parent.end - expr.end)]
-    });
+    var previewInfo = run({expr.parent: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => a | (b | c);');
   }
 
   void test_removeAs_raise_precedence_no_parens_to_remove() async {
-    await resolveTestUnit('''
-f(a, b, c) => a = b | c as int;
-''');
+    await analyze('f(a, b, c) => a = b | c as int;');
     var expr = findNode.binary('b | c');
-    var previewInfo = _run({expr.parent: const RemoveAs()});
-    expect(previewInfo, {
-      expr.end: [RemoveText(expr.parent.end - expr.end)]
-    });
+    var previewInfo = run({expr.parent: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => a = b | c;');
   }
 
   void test_removeAs_raise_precedence_remove_parens() async {
-    await resolveTestUnit('''
-f(a, b, c) => a < (b | c as int);
-''');
+    await analyze('f(a, b, c) => a < (b | c as int);');
     var expr = findNode.binary('b | c');
-    var previewInfo = _run({expr.parent: const RemoveAs()});
-    expect(previewInfo, {
-      expr.parent.parent.offset: [RemoveText(1)],
-      expr.end: [RemoveText(expr.parent.end - expr.end)],
-      expr.parent.end: [RemoveText(1)]
-    });
+    var previewInfo = run({expr.parent: const RemoveAs()});
+    expect(previewInfo.applyTo(code), 'f(a, b, c) => a < b | c;');
   }
 }
 
 class FixPlannerTestBase extends AbstractSingleUnitTest {
-  Map<int, List<PreviewInfo>> _run(Map<AstNode, Change> changes,
+  String code;
+
+  Future<void> analyze(String code) async {
+    this.code = code;
+    await resolveTestUnit(code);
+  }
+
+  Map<int, List<PreviewInfo>> run(Map<AstNode, Change> changes,
       {bool allowRedundantParens = false}) {
-    // TODO(paulberry): test that redundant parens are allowed in certain
-    //  circumstances.
     return FixPlanner.run(testUnit, changes,
         allowRedundantParens: allowRedundantParens);
   }
